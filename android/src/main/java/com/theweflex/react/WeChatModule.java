92831729f3ca702d9d11a8cf615a6aa8e6f4e166
lihuanke <lihuanke@sensetime.com>
2022-12-01 19:47:23 +0800

    fix:不在后台时无法拉起app

--- android/src/main/java/com/theweflex/react/WeChatModule.java
+++ android/src/main/java/com/theweflex/react/WeChatModule.java
@@ -66,6 +66,7 @@ import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.UUID;
+import android.util.Log;
 
 public class WeChatModule extends ReactContextBaseJavaModule implements IWXAPIEventHandler {
     private String appId;
@@ -951,6 +952,7 @@ public class WeChatModule extends ReactContextBaseJavaModule implements IWXAPIEv
         this.getReactApplicationContext()
                 .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                 .emit("WeChat_Resp", map);
+        Log.d("wechat-lib", "onReq" + map.toString());
     }
 
     @Override
@@ -993,7 +995,7 @@ public class WeChatModule extends ReactContextBaseJavaModule implements IWXAPIEv
         this.getReactApplicationContext()
                 .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                 .emit("WeChat_Resp", map);
-        startActivity(new Intent(this, DeviceEventManagerModule.RCTDeviceEventEmitter.class));
+        Log.d("wechat-lib", "onResp" + map.toString());
     }
 
     private interface ImageCallback {
